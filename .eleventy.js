9bf170be131706dcbafdfa19d3a8c334819d82c1
David Poninski <dponinski@icloud.com>
2023-07-02 08:45:59 -0500

    Getting CSS to work

--- .eleventy.js
+++ .eleventy.js
@@ -1,21 +1,4 @@
-// Don’t forget to `npm install sass`!
-const sass = require("sass");
-
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addTemplateFormats("scss");
-
-  // Creates the extension for use
-  eleventyConfig.addExtension("scss", {
-    outputFileExtension: "css", // optional, default: "html"
-
-    // `compile` is called once per .scss file in the input directory
-    compile: async function(inputContent) {
-      let result = sass.compileString(inputContent);
-
-      // This is the render function, `data` is the full data cascade
-      return async (data) => {
-        return result.css;
-      };
-    }
-  });
-};
+    eleventyConfig.addPassthroughCopy("css/styles.css");
+    eleventyConfig.addPassthroughCopy("css/brand.css");
+  };
